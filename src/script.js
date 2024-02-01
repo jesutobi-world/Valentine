f47868ebba62d6556662e8a260a68b43bf93d70e
Bada Jesutobi <jbada467@stu.ui.edu.ng>
2024-02-01 03:49:50 +0100

    modal working now

--- src/script.js
+++ src/script.js
@@ -1,14 +1,16 @@
-function showModal(imageNumber){
-    const card = document.getElementById(`card${imageNumber}`);
-    const modal = document.getElementById('modal');
+let modal;
+let card;
+
+function showModal(imageNumber) {
+    card = document.getElementById(`card${imageNumber}`);
+    modal = document.getElementById('modal');
     modal.classList.remove('hidden');
     card.classList.remove('hidden');
-    
 }
 
 window.onclick = function(event) {
     if (event.target == modal) {
-        card.classList.add('hidden');
         modal.classList.add('hidden');
+        card.classList.add('hidden');
     }
-}
+};
