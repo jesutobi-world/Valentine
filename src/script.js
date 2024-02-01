adb141cbb7971982c2fe6106a90d872ad51f0fcd
Bada Jesutobi <jbada467@stu.ui.edu.ng>
2024-02-01 15:20:16 +0100

    modal...

--- src/script.js
+++ src/script.js
@@ -1,15 +1,18 @@
 let modal;
 let card;
 let close = document.getElementById('close');
+let body = document.getElementById('body');
 
 function showModal(imageNumber) {
     card = document.getElementById(`card${imageNumber}`);
     modal = document.getElementById('modal');
     modal.classList.remove('hidden');
     card.classList.remove('hidden');
+    body.classList.add('overflow-hidden');
 }
 
 close.onclick = function(event) {
         modal.classList.add('hidden');
         card.classList.add('hidden');
+        body.classList.remove('overflow-hiden');
 };
